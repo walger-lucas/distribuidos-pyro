324d77e4bb0e5c1368d01459d20ab83ee9481715
Antonio Ferreira <profissionalantoniofferreira@gmail.com>
2025-05-12 22:54:59 -0300

    Inicio da implementação do cliente

---
+++ project/client.py
@@ -0,0 +1,65 @@
+# saved as greeting-client.py
+import threading
+import Pyro5.api
+
+class Client(threading.Thread):
+    files = {}
+    lock = None
+
+    def run(self):
+        '''
+        while(True):
+            uri = input("Informe a URI do servidor:").strip()
+            filename = input("Informe o nome do arquivo:").strip()
+
+            peer = Pyro5.api.Proxy(uri)
+            print("Arquivo retornado: " + peer.getFile(filename))
+            print("\n\n\n\n")
+        '''
+        self.lock = threading.Lock()
+
+        while(True):
+            selected_option = input(
+                "Selecione uma opção:\n"+
+                "[1] Compartilhar arquivo\n"+
+                "[2] Consultar arquivos\n"+
+                "[3] Baixar arquivo\n\n" +
+                "Escolhas: "
+            )
+
+            print("\n\n\n")
+
+            match(selected_option):
+                case 1:
+                    file_path = input("Informe o local do arquivo: ")
+                    file_name = input("Informe o nome de compartilhamento do arquivo: ")
+
+                    self.add_file(file_path, file_name)
+
+                case 2:
+                    self.list_files()
+
+                case 3:
+                    uri = input("Informe a URI do peer: ")
+                    file_name = input("Informe o nome do arquivos: ")
+    
+    def add_file(self, name, path):
+        with open(path, "rb") as file:
+            file_bytes = file.read()
+        
+        with self.lock():
+            self.file["name"] = file_bytes
+
+    
+
+    def list_files(self):
+        pass
+
+
+    def download_file(self, uri, file_name):
+        file_bytes = Pyro5.api.Proxy(uri).get_file(file_name)
+        
+        with open(f"downloads/{file_name}", "wb") as file:
+            file.write(file_bytes)
+
+        print("Arquivo baixado com sucesso!")
